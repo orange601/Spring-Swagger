8ef7c79e76ca24e3de8df226c984947deff06069
orange <oltop@naver.com>
2022-12-21 17:46:23 +0900

    Update WebConfig.java

--- WebConfig.java
+++ WebConfig.java
@@ -24,19 +24,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedHeaders("*")
-        .exposedHeaders("/**")
-        .allowedMethods(
-    		HttpMethod.GET.name(),
-        	HttpMethod.HEAD.name(),
-        	HttpMethod.POST.name(),
-        	HttpMethod.PUT.name(),
-        	HttpMethod.DELETE.name());
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	    .allowedOrigins("*")
+	    .allowedHeaders("*")
+	    .exposedHeaders("/**")
+	    .allowedMethods(
+			HttpMethod.GET.name(),
+	    	HttpMethod.HEAD.name(),
+	    	HttpMethod.POST.name(),
+	    	HttpMethod.PUT.name(),
+	    	HttpMethod.DELETE.name());
+	}
     
     
     
